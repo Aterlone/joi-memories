29486fdfc466dca5368c6687dba0fee012a8e283
Aterlone <taneahaines@gmail.com>
2025-03-29 13:07:27 +1300

    move image

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const expressLayouts = require('express-ejs-layouts');
 
 const initializePassport = require('./passport-config');
 
-
 const users = []
 
 const app = express();
@@ -36,51 +35,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 
-// app.use(flash())
-
-// //App login session tokens and stuff for passport.
-// app.use(session({
-//     key: "session",
-//     secret: process.env.SESSION_SECRET,
-//     store: new MYSQLStore({
-//         host: "localhost",
-//         port: 3306,
-//         user: "root",
-//         password: process.env.SQLPassword,
-//         database: "cookie_user"
-//     }),
-//     resave: false,
-//     saveUnitialized: false,
-//     cookie: {
-//         maxAge: 1000 * 60 * 60 * 24
-//     }
-// }));
-
-// //Passport stuff
-// app.use(passport.initialize())
-// app.use(passport.session())
-// app.use(methodOverride('_method'))
 
 const render = (res, page, locals = {}) => {
     res.render(page, locals);
 };
 
-//Connection for sql
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: process.env.SQLPassword,
-//     database: "user"
-// });
-
-// initializePassport(
-//     passport,
-//     connection
-// )
-
 //Landing pages
-app.get("/", (req, res) => render(res, "index"));
+app.get("/", (req, res) => {
+    render(res, "index")
+
+});
 // app.get("/pricing", (req, res) => render(res, "pricing"));
 app.get("/about", (req, res) => render(res, "about"));
 app.get("/resources", (req, res) => render(res, "resources"));
@@ -96,90 +60,6 @@ app.get("/client", (req, res) => {
 app.get("/account", (req, res) => {
     render(res, "account")
 });
-
-// app.post("/account", (req, res) => {
-//     const firstname = req.body.firstname.substr(0, 49);
-//     const lastname = req.body.lastname.substr(0, 49);
-//     const email = req.body.email.substr(0, 250);
-//     const bio = req.body.bio.substr(0, 5000);
-//     connection.query('UPDATE users SET firstname = ?, lastname = ?, email = ?, bio = ? WHERE id = ?', [firstname, lastname, email, bio, req.user[0].id], function (err, response, fields) {
-//         if (err) {
-//             return next(err)
-//         }
-//         res.redirect("/account");
-//     });
-// });
-
-
-// //Sign up, login, logout pages/procedures.
-// app.get("/create-organisation", (req, res) => render(res, "create-organisation"));
-// app.post("/create-organisation", (req, res) => render(res, "create-organisation"));
-
-// app.get("/register", (req, res) => render(res, "register"));
-
-// app.post("/register", async (req, res, next) => {
-//     try {
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//         const firstname = req.body.firstname.substr(0, 49);
-//         const lastname = req.body.lastname.substr(0, 49);
-//         const email = req.body.email.substr(0, 250);
-
-//         connection.query(
-//             'INSERT INTO users (firstname, lastname, email, hash) VALUES (?, ?, ?, ?)',
-//             [firstname, lastname, email, hashedPassword],
-//             function (err, response) {
-//                 if (err) {
-//                     return next(err);  // Pass error to Express error handler
-//                 }
-
-//                 // Manually authenticate the user after successful registration
-//                 passport.authenticate('local', {
-//                     successRedirect: "/create-organisation",
-//                     failureRedirect: '/login',
-//                     failureFlash: true
-
-//                 })(req, res, next);
-//             }
-//         );
-//     } catch (error) {
-//         next(error); // Handle bcrypt hashing errors
-//     }
-// });
-
-// app.get('/login', (req, res) => render(res, "login"))
-
-// app.post('/login', passport.authenticate('local', {
-//     successRedirect: "/client?page=dashboard",
-//     failureRedirect: '/login',
-//     failureFlash: true
-
-// }));
-
-// app.delete('/logout', (req, res, next) => {
-//     req.logout(err => {
-//         if (err) {
-//             return next(err);
-//         }
-//         res.redirect('/login');
-//     });
-// });
-
-
-// functio(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return next()
-//     }
-
-//     res.redirect("/login")
-// }
-
-// functio(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return res.redirect("/client?page=dashboard")
-//     }
-
-//     next()
-// }
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,30 @@
-document.addEventListener("scroll", function () {
-    let navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("scrolled", window.scrollY > navbar.offsetHeight);
+
+// Handle window resize
+function handleResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+window.addEventListener('resize', handleResize);
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const element = document.querySelector('.container .element');
+
+    if (!element) {
+        console.error('Element not found!');
+        return;
+    }
+
+    // Mouse movement event to rotate the image
+    window.addEventListener('mousemove', function (event) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        const rotateX = (mouseY / window.innerHeight - 0.5) * 50;
+        const rotateY = (mouseX / window.innerWidth - 0.5) * 50;
+        element.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    let sidebar = document.getElementById("collapsableSidebar");
-    let content = document.querySelector(".content");
 
-    sidebar.addEventListener("hidden.bs.collapse", function () {
-        document.body.classList.add("collapsed-sidebar");
-    });
-
-    sidebar.addEventListener("shown.bs.collapse", function () {
-        document.body.classList.remove("collapsed-sidebar");
-    });
-});
